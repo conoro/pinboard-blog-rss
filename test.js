20168dadbb7a42a6cddca0bfeac4abf3f6f28c0e
Conor ONeill <conor@conoroneill.com>
2020-07-11 09:25:41 +0100

    works but only shows one story because the Pinboard blog is a mess

---
+++ test.js
@@ -0,0 +1,48 @@
+// pinboard-blog-rss - Copyright Conor O'Neill 2020, conor@conoroneill.com
+// LICENSE Apache-2.0
+// Invoke like https://url.of.serverless.function/dev/rss
+
+    var request = require("request");
+    var cheerio = require("cheerio");
+    var RSS = require("rss");
+    var sectionURL = "https://blog.pinboard.in";
+  
+    var feed = new RSS({
+      title: "Pinboard Blog RSS",
+      description: "Get latest Pinboard blogposts as Maciej's link is broken",
+      feed_url: "http://example.com/rss.xml",
+      site_url: "https://blog.pinboard.in/",
+      image_url: "https://pinboard.in/bluepin.gif",
+      docs: "http://example.com/rss/docs.html",
+      managingEditor: "conor@conoroneill.com",
+      webMaster: "conor@conoroneill.com",
+      copyright: "2020 Conor ONeill",
+      language: "en",
+      pubDate: "Jul 10, 2020 08:00:00 GMT",
+      ttl: "60"
+    });
+  
+    request(sectionURL, function(error, response, html) {
+      if (!error && response.statusCode == 200) {
+        var $ = cheerio.load(html);
+        $(".blog_title").each(function() {
+          var link = sectionURL + $(this).attr("href");
+          if (link.indexOf(sectionURL) > -1) {
+            console.log(link);
+            var title = $(this).text();
+            console.log(title);
+            var currentDate = new Date();
+            feed.item({
+              title: title,
+              description: title,
+              url: link,
+              author: "maciej@example.com",
+              date: currentDate
+            });
+          }
+        });
+        var xml = feed.xml();
+       // context.succeed(xml);
+      }
+    });
+  
